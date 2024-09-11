36cba38938114323537cb728e0529ad1c53dd39e
rchandler-eb <rchandler@eventbrite.com>
2024-09-11 15:46:15 -0500

    adds post updates

--- app/(blog)/posts/[slug]/page.tsx
+++ app/(blog)/posts/[slug]/page.tsx
@@ -11,6 +11,7 @@ import { postQuery, settingsQuery } from "@/sanity/lib/queries";
 import { resolveOpenGraphImage } from "@/sanity/lib/utils";
 import PostHeader from "./postHeader";
 import PostBody from "./postBody";
+import Link from "next/link";
 
 type Props = {
   params: { slug: string };
@@ -64,13 +65,29 @@ export default async function PostPage({ params }: Props) {
         <PostBody {...post} />
       </section>
       <aside className="pb-32 small:pl-4 small:pr-4 sm:pl-4 sm:pr-4 lg:pl-40 lg:pr-40">
-        <hr className="border-accent-2 mb-24 mt-28" />
-        <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
-          Recent Stories
-        </h2>
+        <hr className="border-accent-2 mb-24" />
+        <div className="flex flex-row w-full">
+          <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
+            Recent Stories
+          </h2>
+          <div className="flex justify-end ml-auto hidden md:flex">
+            <div className="kura_tm_button text-center sm:w-full md:w-fit h-auto clear-both">
+              <Link className="rounded-full" href="/posts">
+                View All Posts
+              </Link>
+            </div>
+          </div>
+        </div>
         <Suspense>
           <MoreStories skip={post._id} limit={2} />
         </Suspense>
+        <div className="flex ml-auto flex md:hidden mt-16">
+            <div className="kura_tm_button text-center sm:w-full md:w-fit h-auto clear-both">
+              <Link className="rounded-full" href="/posts">
+                View All Posts
+              </Link>
+            </div>
+          </div>
       </aside>
     </div>
   );
